19caf42fdf0bc7ae9e9bff024007a5fa142e3ffe
네모 <153412661+nemonemoname@users.noreply.github.com>
2023-12-14 22:15:55 +0900

    Update wordgenerator.py
    코드 전반적으로 수정

--- wordgenerator.py
+++ wordgenerator.py
@@ -1,12 +1,17 @@
 import random
 
-def generate_random_word(consonants, vowels, syllable_structure, min_length, max_length):
+def generate_random_word(consonants, vowels, min_length, max_length):
     word = ""
     length = random.randint(min_length, max_length)
 
+    syllable_structures = [
+        ['C', 'V'],
+        ['C', 'V', 'C']
+    ]
+
     for _ in range(length):
-        syllable = generate_syllable(consonants, vowels, syllable_structure)
-        word += syllable
+        chosen_structure = random.choice(syllable_structures)
+        word += generate_syllable(consonants, vowels, chosen_structure)
 
     return word.capitalize()
 
@@ -17,19 +22,15 @@ def generate_syllable(consonants, vowels, syllable_structure):
             syllable += random.choice(consonants)
         elif s == 'V':
             syllable += random.choice(vowels)
+
     return syllable
 
-def generate_random_words(consonants, vowels, syllable_structure, min_length, max_length, num_words):
-    words = [generate_random_word(consonants, vowels, syllable_structure, min_length, max_length) for _ in range(num_words)]
-    return words
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'z']
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'z'] #Consonants
-vowels = ['a', 'e', 'i', 'o', 'u'] #Vowels
-syllable_structure = ['C', 'V']  # Consonant followed by Vowel
+min_length = 1
+max_length = 3
+num_words = 5
 
-min_length = 3 #Minimum lengths of words
-max_length = 8 #Max lengths of words
-num_words = 5  # Adjust the number of words to generate
-
-random_words = generate_random_words(consonants, vowels, syllable_structure, min_length, max_length, num_words)
+random_words = [generate_random_word(consonants, vowels, min_length, max_length) for _ in range(num_words)]
 print("Random Words:", random_words)
